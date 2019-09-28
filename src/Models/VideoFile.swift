c76ae3a9b952605965df6961e4bcfb6c1f8b688b
Mounir Ybanez <rinuom91@gmail.com>
2019-09-28 10:58:41 +0800

    Added VideoFile model.

---
+++ src/Models/VideoFile.swift
@@ -0,0 +1,17 @@
+//
+//  VideoFile.swift
+//  Peekr
+//
+//  Created by Mounir Ybanez on 9/28/19.
+//  Copyright © 2019 Nir. All rights reserved.
+//
+
+import Foundation
+
+public struct VideoFile {
+    
+    public let id: String
+    public let height: Double
+    public let width: Double
+    public let downloadURLString: String
+}
